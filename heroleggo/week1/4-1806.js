e9176a22dd0ace63e9460fc04544e159991049ce
Youngwoong You <asdfg13232@naver.com>
2022-11-24 17:27:47 +0900

    feat: Add problem 4

--- heroleggo/week1/4-1806.js
+++ heroleggo/week1/4-1806.js
@@ -1,40 +1,30 @@
 const fs = require('fs');
 
-const [[N, S], nums] = `10 15
-5 1 3 5 10 7 4 9 2 8`.split('\n').map((v) => v.split(' ').map((v) => +v)) //fs.readFileSync('/dev/stdin').toString().trim().split('\n').map((v) => v.split(' ').map((v) => +v));
+const [[N, S], nums] = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map((v) => v.split(' ').map((v) => +v));
 
-const divideAndConquer = (data, start, end) => {
-    let result = -1;
+let start = 0;
+let end = 0;
 
-    // base condition
-    if (start === end) {
-        return data[start];
+let result = Number.MAX_SAFE_INTEGER;
+
+let sum = 0;
+
+while (start < N) {
+    if (sum >= S) { // 조건 충족 시
+        if (end - start < result) {
+            result = end - start;
+        }
+        sum -= nums[start];
+        start++;
+    } else {
+        if (end !== N) {
+            sum += nums[end];
+            end++;
+        } else { // start 인덱스를 증가시켜야 함
+            sum -= nums[start];
+            start++;
+        }
     }
-
-    const mid = (start + end) % 2 ? (start + end + 1) / 2 : (start + end) % 2;
-
-    let left = -1;
-    let right = -1;
-
-    let leftSum = 0;
-    let rightSum = 0;
-
-    for (let i = mid; i >= start; i--) {
-        leftSum += data[i];
-        left = Math.max(left, leftSum);
-    }
-
-    for (let i = mid + 1; i < end; i++) {
-        rightSum += data[i];
-        right = Math.max(right, rightSum);
-    }
-
-    result = Math.max(divideAndConquer(data, start, mid), divideAndConquer(data, mid + 1, end));
-    result = Math.max(result, left + right);
-
-    return result;
 }
 
-const getSubsequence = (data, criteria) => {
-
-}
+console.log(result === Number.MAX_SAFE_INTEGER ? 0 : result);
